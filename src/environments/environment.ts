795e7711a9534c49228cc1172dde6bb7b1b4e442
Amine0702 <amineabdallah2k22@gmail.com>
2026-06-17 22:24:17 +0200

    fix apiUrl parentheses in environment.ts + add fileReplacements for prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "(https://bydjobackend-vmix.onrender.com/api",
+  apiUrl: "https://bydjobackend-vmix.onrender.com/api",
   storeName: "BY DJO",
   currency: "TND",
   currencySymbol: "د.ت",
